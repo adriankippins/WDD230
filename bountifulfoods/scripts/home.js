0deb78320626b007edb7f3ef7133238831d4fc2e
Roberto Kippins <robertokippins@iotgy.net>
2023-07-03 06:29:03 -0400

    Bountiful Foods Home Page Review 1.0

--- bountifulfoods/scripts/home.js
+++ bountifulfoods/scripts/home.js
@@ -16,11 +16,50 @@ document.addEventListener('DOMContentLoaded', (event) => {
         detailsElement.innerHTML = `Family: ${drink.family}<br>Order: ${drink.order}<br>Calories: ${drink.nutritions.calories}`;
         drinkElement.appendChild(detailsElement);
 
+        const tableElement = createNutritionalFactsTable(drink.nutritions);
+        drinkElement.appendChild(tableElement);
+
         specialDrinksSection.appendChild(drinkElement);
       });
     })
     .catch(error => console.error('Error:', error));
 });
+
+function createNutritionalFactsTable(nutritions) {
+  const tableElement = document.createElement('table');
+  tableElement.classList.add('nutritional-facts');
+
+  const tableBody = document.createElement('tbody');
+  const totalCaloriesRow = createTableRow('Total Calories', nutritions.calories);
+  const carbohydratesRow = createTableRow('Carbohydrates', nutritions.carbohydrates);
+  const proteinRow = createTableRow('Protein', nutritions.protein);
+  const fatRow = createTableRow('Fat', nutritions.fat);
+  const sugarRow = createTableRow('Sugar', nutritions.sugar);
+
+  tableBody.appendChild(totalCaloriesRow);
+  tableBody.appendChild(carbohydratesRow);
+  tableBody.appendChild(proteinRow);
+  tableBody.appendChild(fatRow);
+  tableBody.appendChild(sugarRow);
+
+  tableElement.appendChild(tableBody);
+
+  return tableElement;
+}
+
+function createTableRow(label, value) {
+  const row = document.createElement('tr');
+  const labelCell = document.createElement('td');
+  const valueCell = document.createElement('td');
+
+  labelCell.textContent = label;
+  valueCell.textContent = value;
+
+  row.appendChild(labelCell);
+  row.appendChild(valueCell);
+
+  return row;
+}
 
 
 // Weather Data

d4d39fc46fdfed695c6c39968c33697129f62e19
adriankippins <robertokippins@iotgy.net>
2023-07-05 19:01:31 -0400

    BF Home Page Review

--- bountifulfoods/scripts/home.js
+++ bountifulfoods/scripts/home.js
@@ -2,52 +2,36 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad,us&units=metr
   .then(response => response.json())
   .then(data => {
     const forecastList = data.list;
-
-    // Get the current date
     const currentDate = new Date();
-
-    // Get the container element for the forecast details
     const weatherArea = document.getElementById('weather-area');
-
-    // Create an array to store unique forecast dates
     const uniqueDates = [];
 
-    // Iterate over the forecast list and populate the forecast details
     forecastList.forEach(forecast => {
-      // Get the date of the forecast
       const forecastDate = new Date(forecast.dt * 1000);
       const forecastDateString = forecastDate.toDateString();
 
-      // Check if the forecast date is greater than or equal to the current date
       if (forecastDate >= currentDate && !uniqueDates.includes(forecastDateString)) {
-        // Add the forecast date to the unique dates array
         uniqueDates.push(forecastDateString);
-
         const dayName = forecastDate.toLocaleDateString('en-US', { weekday: 'long' });
         const monthName = forecastDate.toLocaleDateString('en-US', { month: 'long' });
         const dayOfMonth = forecastDate.toLocaleDateString('en-US', { day: 'numeric' });
 
-        // Get the temperature and weather conditions
         const temperature = Math.round(forecast.main.temp);
         const conditions = capitalizeFirstLetter(forecast.weather[0].description);
 
-        // Get the weather icon code
         const iconCode = forecast.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        // Create a forecast card element
         const forecastCard = document.createElement('div');
         forecastCard.classList.add('forecast-card');
 
-        // Populate the forecast card with the date, temperature, conditions, and icon
         forecastCard.innerHTML = `
           <div class="forecast-date">${dayName}, ${monthName} ${dayOfMonth}</div>
           <div class="forecast-temperature">${temperature}°C</div>
           <div class="forecast-conditions">${conditions}</div>
-          <img src="${iconUrl}" alt="Weather Icon">
+          <img src="${iconUrl}" alt="Icon representing ${conditions}">
         `;
 
-        // Append the forecast card to the weather area container
         weatherArea.appendChild(forecastCard);
       }
     });
@@ -62,11 +46,12 @@ function capitalizeFirstLetter(str) {
 
 
 
-
-//Js for Submitted drink count
 document.addEventListener('DOMContentLoaded', (event) => {
   const specialDrinksSection = document.querySelector('#special-drinks');
-  const submittedDrinksCountElement = document.querySelector('#submitted-drinks-count');
+  const submittedDrinksContainer = document.querySelector('#submitted-drinks-count');
+
+  // Create a new h2 element
+  let submittedDrinksCountElement = document.createElement('h2');
 
   // Retrieve the submitted drink count from local storage or initialize it to 0
   let submittedDrinksCount = localStorage.getItem('submittedDrinksCount');
@@ -76,6 +61,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   // Display the submitted drink count
   submittedDrinksCountElement.textContent = `Total Submitted Drinks: ${submittedDrinksCount}`;
+
+  // Append the h2 element to the submittedDrinksContainer
+  submittedDrinksContainer.appendChild(submittedDrinksCountElement);
 
   fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
     .then(response => response.json())
@@ -93,6 +81,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     })
     .catch(error => console.error('Error:', error));
 });
+
 
 
 

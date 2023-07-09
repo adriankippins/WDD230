e5b7ffd7139fc70d8333fccb74b5d837e4ce48b9
Roberto Kippins <robertokippins@iotgy.net>
2023-07-08 21:15:13 -0400

    BF Site Review

--- bountifulfoods/scripts/main.js
+++ bountifulfoods/scripts/main.js
@@ -38,3 +38,23 @@ document.addEventListener("DOMContentLoaded", () => {
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+//Lazy Loading
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll(".lazyload");
+  var imageObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        var image = entry.target;
+        image.src = image.dataset.src;
+        image.classList.remove("lazyload");
+        imageObserver.unobserve(image);
+      }
+    });
+  });
+
+  lazyloadImages.forEach(function(image) {
+    imageObserver.observe(image);
+  });
+});
